da757ed1abf85b39ea43b4a8f45a76a9c0e50021
Knaxpunkt <regina.mulsow@googlemail.com>
2026-09-17 01:06:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='emom-reps-v1-2-2';
+const CACHE='emom-reps-v1-3-0';
 const ASSETS=['./round.wav','./warning.wav','./finish.wav','./icon_pushups.jpg','./icon_pullups.jpg','./icon_australian.jpg','./icon_squats.jpg','./icon_knee.jpg','./icon_leg.jpg','./icon_dips.jpg','./icon_plank.jpg','./icon_support_pullups.jpg','./icon_support_squats.jpg','./','./index.html','./manifest.webmanifest','./gym.jpg','./prison.jpg','./concrete.jpg','./dark.jpg','./minimal.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
