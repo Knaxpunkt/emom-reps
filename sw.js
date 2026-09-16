def0869b7b4048045dc6d4504ec8776a626b4813
Knaxpunkt <regina.mulsow@googlemail.com>
2026-09-16 20:37:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='emom-reps-v1-2-1';
-const ASSETS=['./round.wav','./warning.wav','./finish.wav','./icon_pushups.jpg','./icon_pullups.jpg','./icon_australian.jpg','./icon_squats.jpg','./icon_knee.jpg','./icon_leg.jpg','./icon_dips.jpg','./icon_plank.jpg','./','./index.html','./manifest.webmanifest','./gym.jpg','./prison.jpg','./concrete.jpg','./dark.jpg','./minimal.jpg'];
+const CACHE='emom-reps-v1-2-2';
+const ASSETS=['./round.wav','./warning.wav','./finish.wav','./icon_pushups.jpg','./icon_pullups.jpg','./icon_australian.jpg','./icon_squats.jpg','./icon_knee.jpg','./icon_leg.jpg','./icon_dips.jpg','./icon_plank.jpg','./icon_support_pullups.jpg','./icon_support_squats.jpg','./','./index.html','./manifest.webmanifest','./gym.jpg','./prison.jpg','./concrete.jpg','./dark.jpg','./minimal.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
